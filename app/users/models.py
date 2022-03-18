8f71df71c30e206f407a49b34a1f1c916f0fbaf4
Jacob Leiner <jacob.leiner@gmail.com>
2022-03-17 18:52:08 -0700

    completely forgot to commit.  dockerized app with custom user model

---
+++ app/users/models.py
@@ -0,0 +1,26 @@
+from uuid import uuid4
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+    dob = models.DateField(_('date of birth'), null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return self.get_full_name()
